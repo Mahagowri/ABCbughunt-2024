0cf8d4861734e177b7f9b1273165fed24666631e
Mahagowri <132816636+Mahagowri@users.noreply.github.com>
2024-03-16 11:01:26 +0530

    Update p11.py

--- py/p11.py
+++ py/p11.py
@@ -3,7 +3,7 @@
 def binaryconversion(n):
    print(n % 2,end = '')
    if n > 1:
-       binaryconversion(n/2)
+       binaryconversion(n//2)
 
 number=int(input("Enter Number: "))
 binaryconversion(number)
