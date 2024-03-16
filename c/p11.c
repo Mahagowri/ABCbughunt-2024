8c6d6782f1f47e3bd29467a837ad2928c30cd730
Mahagowri <132816636+Mahagowri@users.noreply.github.com>
2024-03-16 11:29:38 +0530

    Update p11.c

--- c/p11.c
+++ c/p11.c
@@ -1,6 +1,8 @@
 //program to check if two binary trees are the same, given the roots
 
 #include <stdio.h>
+#include <stdlib.h>
+
 //Definition for a binary tree node.
 struct TreeNode {
     int val;
@@ -12,8 +14,8 @@ int isSameTree(struct TreeNode* p, struct TreeNode* q){
     if(p == NULL && q == NULL){
         return 1;
     }
-    if(p->val == q->val){
-        return isSameTree(p->left, q->left) || isSameTree(p->right, q->right);
+    if(p != NULL && q != NULL && p->val == q->val){
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
     return 0;
 }
